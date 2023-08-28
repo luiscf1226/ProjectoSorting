277b8df81ed2e2b76ce6930f620527e61a25cb24
Luis Flores <luiscf1226@gmail.com>
2023-08-27 20:32:56 -0600

    drawing basics

--- Game/game.py
+++ Game/game.py
@@ -3,9 +3,7 @@ import time
 import random
 
 #initialize
-#bars1
-bar_positions = []
-#
+
 pygame.init()
 #width and height for screen
 width,height=800,700
@@ -23,6 +21,79 @@ background.set_alpha(alpha)
 
 #RGB colors
 white = (255, 255, 255)
+#bars1
+bar_positions1 = []
+#bars1
+bar_positions2 = []
+#array bars 1
+bar_array1=[]
+#array bars 2
+bar_array2=[]
+#area 1
+bar_area1 = pygame.Rect(width // 4, 190, 540, 220)
+#area 2
+bar_area2 = pygame.Rect(width//4,440, 540, 220)
+def quicksort(arr,bar_positions,bar_area):
+    #check if its less than 2 dont need to order array
+    if len(arr) < 2:
+        return arr
+
+    #pivot element at first elememt
+    pivot = arr[0]
+    
+    #left and right arrays
+    leftArray = []
+    rightArray = []
+    
+    #assign resulting left if smaller right if bigger than pivot element
+    for i in range(1, len(arr)):
+        if arr[i] < pivot:
+            leftArray.append(arr[i])
+        else:
+            rightArray.append(arr[i])
+
+    #recursively order left and right, this creates more lefts and rights
+    leftSort = quicksort(leftArray,bar_positions,bar_area)
+    rightSort = quicksort(rightArray,bar_positions,bar_area)
+    
+    #add to array left middle pivot and right
+    result = leftSort + [pivot] + rightSort
+    
+    # Update arr with the sorted result
+    #this lets the array be updated each recursion iteration
+    for i in range(len(result)):
+        arr[i] = result[i]
+
+    # Update bar_positions with new heights
+    bar_positions = updateBarPositions(result,bar_area)       
+   
+    # Redraw the screen to see changes reflected in bar_positions
+    drawSetup()
+    drawBarsBox1(bar_positions, result)        
+    
+    pygame.time.wait(500)  # delay of 0.5 seconds
+    pygame.display.update()
+    return result
+
+def bubblesort(random_numbers,bar_positions,bar_area):
+    n = len(random_numbers)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            #swap if next element in array is greather 
+            if random_numbers[j] > random_numbers[j+1]:
+                random_numbers[j], random_numbers[j+1] = random_numbers[j+1], random_numbers[j]
+                
+                # Update bar_positions with new heights
+                bar_positions = updateBarPositions(random_numbers,bar_area)
+                
+                # Redraw the screen to see changes reflected in bar_positions
+                drawSetup()
+                drawBarsBox2(bar_positions, random_numbers)
+                  
+                pygame.time.wait(500)  # delay of0.5 seconds
+                
+    pygame.display.update()
+    
 
 def drawMainTitle():
     #Title object
@@ -101,21 +172,21 @@ def drawSort2Title():
     # set coordinates for text
     textTitleSurface5.midtop=(width//6,480)
     window.blit(textTitle5,textTitleSurface5)
-def drawSort2Box():
-    pygame.draw.rect(window, white, [width//4,440, 540, 220])
+
 def drawSort1Box():
+    #draws rectangle 1
     pygame.draw.rect(window, white, [width//4,190, 540, 220])
-def drawBarsBox1(bar_positions):
+def drawBarsBox1(bar_positions,bar_heights):
     color_red = (170, 0, 0)
     bar_width = 10
-
-    for bar_x, bar_y, bar_height in bar_positions:
+    #gets x,y coordinates and height to be drawn from bars arrays
+    for (bar_x, bar_y),bar_height in zip(bar_positions,bar_heights):
         pygame.draw.rect(window, color_red, [bar_x, bar_y, bar_width, bar_height])
-    pygame.display.update()
+    pygame.display.update()   
 def insertBars1():
     #set bar area to the rectangle 
     bar_width = 10
-    bar_area = pygame.Rect(width // 4, 190, 540, 220)
+   
     num_bars = 5
 
     # distance between bars
@@ -125,13 +196,59 @@ def insertBars1():
     
     for i in range(num_bars):
         #generate x coordinate
-        bar_x = bar_area.left + i * (bar_width + distance_between_bars)
+        bar_x = bar_area1.left + i * (bar_width + distance_between_bars)
         #random height
         bar_height = random.randint(50, 130)
         #generate y coordinate
-        bar_y = bar_area.bottom - bar_height
-        bar_positions.append((bar_x, bar_y, bar_height))
+        bar_y = bar_area1.bottom - bar_height
+        bar_positions1.append((bar_x, bar_y))
+        bar_array1.append(bar_height)
+def drawSort2Box():
+    pygame.draw.rect(window, white, [width//4,440, 540, 220])
+def drawBarsBox2(bar_positions2,bar_heights2):
+    color_red = (170, 0, 0)
+    bar_width = 10
+    #gets x,y coordinates and height to be drawn from bars arrays
+    for (bar_x, bar_y),bar_height in zip(bar_positions2,bar_heights2):
+        pygame.draw.rect(window, color_red, [bar_x, bar_y, bar_width, bar_height])
+    pygame.display.update()   
+def insertBars2():
+    #set bar area to the rectangle 
+    bar_width = 10
 
+    num_bars = 5
+
+    # distance between bars
+    distance_between_bars = 10
+
+    # Generate bars
+    
+    for i in range(num_bars):
+        #generate x coordinate
+        bar_x = bar_area2.left + i * (bar_width + distance_between_bars)
+        #random height
+        bar_height = random.randint(50, 130)
+        #generate y coordinate
+        bar_y = bar_area2.bottom - bar_height
+        bar_positions2.append((bar_x, bar_y))
+        bar_array2.append(bar_height)
+def updateBarPositions(bar_heights, reference_rect):
+
+    bar_width = 10
+    distance_between_bars = 10
+    new_positions = []
+    #since array changes with each iteration of sorting, it needs to update
+    for i, height in enumerate(bar_heights):
+        #get border of rectangle and add 10 to each bar
+        bar_x = reference_rect.left + i * (bar_width + distance_between_bars)
+        #subtract from bottom border height
+        bar_y = reference_rect.bottom - height
+        new_positions.append((bar_x, bar_y))
+    return new_positions
+
+
+def clearScreen():
+    window.fill((255, 255, 255))  
 def drawSetup():
     window.blit(background, (0, 0))  
 
@@ -152,32 +269,41 @@ def drawSetup():
     drawSort1Box()
 
     drawSort2Box()
+
    
 def main():
-    window.fill(white)
-    
    
     #variable run to keep screen
     run=True
+    sorted1 = False
+    sorted2 = False
+    drawSetup()
+    #insert random bars to first array
     insertBars1()
+     #insert random bars to second array
+    insertBars2()
+  
+    drawBarsBox1(bar_positions1,bar_array1)
+    drawBarsBox2(bar_positions2,bar_array2)
     while run:
         for event in pygame.event.get():
             #user closes window with x click on screen
             if (event.type==pygame.QUIT):
                 run=False
                 break
-        drawBarsBox1(bar_positions)
-        drawSetup()
-      
+
+        if not sorted1:
+            bubblesort(bar_array1,bar_positions1,bar_area1)
+            sorted1 = True  # array ordered so it can stop being sorted
+        #order second array
+        if not sorted2:
+            quicksort(bar_array2,bar_positions2,bar_area2)
+            sorted2 = True  # array ordered so it can stop being sorted
+
+       
 
     pygame.quit()
 
 #run file if its main
 if __name__=="__main__":
     main()
-# Area for the random bars (inside the white box)
-   ## for _ in range(num_bars):
-       # bar_height = random.randint(50, 130)
-       # bar_x = random.randint(bar_area.left, bar_area.right - bar_width)
-       # bar_y = random.randint(bar_area.top, bar_area.bottom - bar_height)
-      #  pygame.draw.rect(window, color_red, [bar_x, bar_y, bar_width, bar_height])
